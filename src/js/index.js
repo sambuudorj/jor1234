ff12f0735431d1028b3a386fbe63cc6f8be8389b
Aagii1234 <aagiiaga631@gmail.com>
2023-10-03 14:22:29 +0800

    search iconiig gargana

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,7 @@
 require("@babel/polyfill");
 import Search from "./model/Search";
+import { elements, renderLoader, clearLoader } from "./view/base";
+import * as searchView from "./view/searchView";
 
 /*
 
@@ -15,23 +17,28 @@ const state = {};
 
 const controlSearch = async () => {
   // 1) Вэбээс хайлтын түлхүүр үгийг гаргаж авна.
-  const query = "pizza";
+  const query = searchView.getInput();
 
   if (query) {
     // 2) Шинээр хайлтын объектыг үүсгэж өгнө
     state.search = new Search(query);
 
     // 3) Хайлт хийхэд зориулж дэлгэцийн UI бэлтгэнэ
+    searchView.clearSearchQuery();
+    searchView.clearSearchResult();
+    renderLoader(elements.searchResultsDiv);
 
     // 4) Хайлтыг гүйцэтгэнэ
     await state.search.doSearch();
 
     // 5) Хайлтын үр дүнг дэлгэцэнд үзүүлнэ
-    console.log(state.search.result);
+    clearLoader();
+    if (state.search.result === undefined) alert("Хайлт илэрцгүй...");
+    else searchView.renderRecipes(state.search.result);
   }
 };
 
-document.querySelector(".search").addEventListener("submit", (e) => {
+elements.searchForm.addEventListener("submit", (e) => {
   e.preventDefault();
   controlSearch();
 });
